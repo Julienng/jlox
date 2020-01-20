359e714050c015f5913e9924fd63e7fd90cd8627
Julien CAVALEIRO <julien@cavaleiro.fr>
2020-01-20 20:56:25 +0100

    add generation for pattern visitor

--- src/org/lox/GenerateAst.kt
+++ src/org/lox/GenerateAst.kt
@@ -7,9 +7,9 @@ import kotlin.system.exitProcess
 fun main(args: Array<String>) {
 	if (args.size != 1) {
 		System.err.println("Usage: generate_ast <output directory>")
-		exitProcess(1);
+		exitProcess(1)
 	}
-	val outputDir = args[0];
+	val outputDir = args[0]
 
 	defineAst(
 		outputDir, "Expr", listOf(
@@ -22,21 +22,26 @@ fun main(args: Array<String>) {
 }
 
 fun defineAst(outputDir: String, baseName: String, types: List<String>) {
-	val path = "$outputDir/$baseName.kt";
+	val path = "$outputDir/$baseName.kt"
 	val writer = PrintWriter(path, "UTF-8")
 
 	writer.println("package org.lox.expr;")
 	writer.println()
-	writer.println("import java.util.List;");
-	writer.println("import org.lox.Token;");
+//	writer.println("import java.util.List;")
+	writer.println("import org.lox.Token")
 	writer.println()
 	writer.println("internal abstract class $baseName {")
+
+	defineVisitor(writer, baseName, types)
 
 	for (type in types) {
 		val className = type.split(":")[0].trim()
 		val fields = type.split(":")[1].trim()
 		defineType(writer, baseName, className, fields)
 	}
+
+	writer.println()
+	writer.println("  abstract fun <R> accept(visitor: Visitor<R>): R")
 
 	writer.println("}")
 	writer.close()
@@ -56,5 +61,20 @@ fun defineType(writer: PrintWriter, baseName: String, className: String, fieldLi
 	}
 
 	writer.print(parameters.joinToString(separator = ",", prefix = "(", postfix = ")") { it -> it })
-	writer.println(" : $baseName()")
+	writer.println(" : $baseName() {")
+	writer.println()
+	writer.println("    override fun <R> accept(visitor: Visitor<R>): R {")
+	writer.println("      return visitor.visit$className$baseName(this)")
+	writer.println("    }")
+	writer.println("  }")
+}
+
+fun defineVisitor(writer: PrintWriter, baseName: String, types: List<String>) {
+	writer.println("  interface Visitor<R> {")
+
+	for (type in types) {
+		val typeName = type.split(":")[0].trim()
+		writer.println("  fun visit$typeName$baseName(${baseName.toLowerCase()} : $typeName): R")
+	}
+	writer.println("  }")
 }
--- src/org/lox/expr/Expr.kt
+++ src/org/lox/expr/Expr.kt
@@ -1,11 +1,42 @@
-package org.lox.expr;
+package org.lox.expr
 
-import java.util.List;
-import org.lox.Token;
+import org.lox.Token
 
 internal abstract class Expr {
-  internal class Binary(val left: Expr,val operator: Token,val right: Expr) : Expr()
-  internal class Grouping(val expression: Expr) : Expr()
-  internal class Literal(val value: Any) : Expr()
-  internal class Unary(val operator: Token,val right: Expr) : Expr()
+	interface Visitor<R> {
+		fun visitBinaryExpr(expr: Binary): R
+		fun visitGroupingExpr(expr: Grouping): R
+		fun visitLiteralExpr(expr: Literal): R
+		fun visitUnaryExpr(expr: Unary): R
+	}
+
+	internal class Binary(val left: Expr, val operator: Token, val right: Expr) : Expr() {
+
+		override fun <R> accept(visitor: Visitor<R>): R {
+			return visitor.visitBinaryExpr(this)
+		}
+	}
+
+	internal class Grouping(val expression: Expr) : Expr() {
+
+		override fun <R> accept(visitor: Visitor<R>): R {
+			return visitor.visitGroupingExpr(this)
+		}
+	}
+
+	internal class Literal(val value: Any) : Expr() {
+
+		override fun <R> accept(visitor: Visitor<R>): R {
+			return visitor.visitLiteralExpr(this)
+		}
+	}
+
+	internal class Unary(val operator: Token, val right: Expr) : Expr() {
+
+		override fun <R> accept(visitor: Visitor<R>): R {
+			return visitor.visitUnaryExpr(this)
+		}
+	}
+
+	abstract fun <R> accept(visitor: Visitor<R>): R
 }
