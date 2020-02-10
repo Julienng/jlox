67693540fb6538702392c3baa6b523e96c4cd70f
Julien CAVALEIRO <julien@cavaleiro.fr>
2020-02-10 23:33:38 +0100

    start implementing closure

--- src/org/lox/LoxFunction.kt
+++ src/org/lox/LoxFunction.kt
@@ -2,12 +2,15 @@ package org.lox
 
 import org.lox.ast.Stmt
 
-class LoxFunction(private val declaration: Stmt.Function) : LoxCallable {
+class LoxFunction(
+    private val declaration: Stmt.Function,
+    private val closure: Environment
+) : LoxCallable {
     override val arity: Int
         get() = declaration.params.size
 
     override fun call(interpreter: Interpreter, arguments: List<Any?>): Any? {
-        val environment = Environment(interpreter.globals)
+        val environment = Environment(closure)
 
         for (i in declaration.params.indices) {
             environment.define(declaration.params[i].lexeme, arguments[i])
