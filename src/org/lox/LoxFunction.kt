faa5af8f44a9c9da9afa8f389d3a01305bb4ac67
Julien CAVALEIRO <julien@cavaleiro.fr>
2020-02-10 22:16:23 +0100

    Correction of index loop with number un kotlin

--- src/org/lox/LoxFunction.kt
+++ src/org/lox/LoxFunction.kt
@@ -3,21 +3,21 @@ package org.lox
 import org.lox.ast.Stmt
 
 class LoxFunction(private val declaration: Stmt.Function) : LoxCallable {
-	override val arity: Int
-		get() = declaration.params.size
+    override val arity: Int
+        get() = declaration.params.size
 
-	override fun call(interpreter: Interpreter, arguments: List<Any?>): Any? {
-		val environment = Environment(interpreter.globals)
+    override fun call(interpreter: Interpreter, arguments: List<Any?>): Any? {
+        val environment = Environment(interpreter.globals)
 
-		for (i in 0..declaration.params.size) {
-			environment.define(declaration.params[i].lexeme, arguments[i])
-		}
+        for (i in declaration.params.indices) {
+            environment.define(declaration.params[i].lexeme, arguments[i])
+        }
 
-		interpreter.executeBlock(declaration.body, environment)
-		return null
-	}
+        interpreter.executeBlock(declaration.body, environment)
+        return null
+    }
 
-	override fun toString(): String {
-		return "<fn ${declaration.name.lexeme} >"
-	}
+    override fun toString(): String {
+        return "<fn ${declaration.name.lexeme} >"
+    }
 }
